563ebc5c2ed633f0f7baa3e6131d96ff7a8dd974
Blessed Zulu <blessedmattzulu@gmail.com>
2026-05-31 19:43:46 +0200

    feat: Banner + Interstitial builders

---
+++ src/Builders/BannerAd.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BlessedZulu\NativePhpAdmob\Builders;
+
+use BlessedZulu\NativePhpAdmob\Admob;
+use BlessedZulu\NativePhpAdmob\Contracts\Bridge;
+use Illuminate\Support\Facades\Log;
+
+class BannerAd
+{
+    public const FORMAT = 'banner';
+
+    public function __construct(
+        protected Bridge $bridge,
+        protected Admob $manager,
+        protected string $slot,
+        protected string $adUnitId,
+    ) {}
+
+    public function load(): self
+    {
+        $this->bridge->call('Admob.LoadBanner', $this->params());
+
+        return $this;
+    }
+
+    public function show(string $position = 'bottom'): self
+    {
+        if (! $this->manager->canRequestAds()) {
+            Log::info('Admob: banner show() skipped, consent not granted.', ['slot' => $this->slot]);
+
+            return $this;
+        }
+
+        $this->bridge->call('Admob.ShowBanner', $this->params(['position' => $position]));
+
+        return $this;
+    }
+
+    public function hide(): self
+    {
+        $this->bridge->call('Admob.HideBanner', $this->params());
+
+        return $this;
+    }
+
+    /**
+     * @param  array<string, mixed>  $extra
+     * @return array<string, mixed>
+     */
+    protected function params(array $extra = []): array
+    {
+        return array_merge([
+            'slot' => $this->slot,
+            'format' => self::FORMAT,
+            'unit_id' => $this->adUnitId,
+        ], $extra);
+    }
+}
---
+++ src/Builders/InterstitialAd.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BlessedZulu\NativePhpAdmob\Builders;
+
+use BlessedZulu\NativePhpAdmob\Admob;
+use BlessedZulu\NativePhpAdmob\Contracts\Bridge;
+use Illuminate\Support\Facades\Log;
+
+class InterstitialAd
+{
+    public const FORMAT = 'interstitial';
+
+    public function __construct(
+        protected Bridge $bridge,
+        protected Admob $manager,
+        protected string $slot,
+        protected string $adUnitId,
+    ) {}
+
+    public function load(): self
+    {
+        $this->bridge->call('Admob.LoadInterstitial', $this->params());
+
+        return $this;
+    }
+
+    public function isReady(): bool
+    {
+        $response = $this->bridge->call('Admob.InterstitialReady', $this->params());
+
+        return (bool) ($response['data']['ready'] ?? false);
+    }
+
+    public function show(): self
+    {
+        if (! $this->manager->canRequestAds()) {
+            Log::info('Admob: interstitial show() skipped, consent not granted.', ['slot' => $this->slot]);
+
+            return $this;
+        }
+
+        $this->bridge->call('Admob.ShowInterstitial', $this->params());
+
+        return $this;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    protected function params(): array
+    {
+        return [
+            'slot' => $this->slot,
+            'format' => self::FORMAT,
+            'unit_id' => $this->adUnitId,
+        ];
+    }
+}
